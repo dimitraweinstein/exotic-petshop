daf326dc986493f37307be91d9322b5c9a084836
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-11 15:11:27 -0700

    added get total price function, worked on cart js

--- cart/cart.js
+++ cart/cart.js
@@ -1,14 +1,33 @@
 import { cartItems } from '../data/cart-data.js';
+import { renderCartItem, getCartTotal } from './cart/render-cart-items.js';
 
-for (let cart of cartItems) {
+const anchor = document.querySelector('tbody');
+const total = document.getElementById('total');
 
+for (let item of cartItems) {
+    const tableRow = renderCartItem(item);
+
+    anchor.append(tableRow);
 }
+
+const totalPrice = getCartTotal();
+
+total.textContent = totalPrice.toLocaleString('en=US,' {
+    style: 'currency',
+    currency: 'USD'
+})
+
+
+
+
+
+
 
 
 // import pets from '../data/pets-data.js';
 // import renderPets from './render-pets.js';
 
-// const list = document.getElementById('pets');
+// const table = querySelector('table');
 
 // for (let i = 0; i < pets.length; i++) {
 //     const pet = pets[i];
